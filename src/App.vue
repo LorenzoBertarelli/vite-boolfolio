ca47f2937f3cb30c2eccf9927bf0288bffeee882
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-06-13 19:37:41 +0200

    milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
-  data() {
-    return {
-      projects: []
-    }
-  },
-  mounted() {
-    this.getProject();
-  },
-  methods: {
-    getProject() {
-      console.log('get projects');
-      axios.get('http://localhost:8000/api/projects').then(resp => {
-        this.projects = resp.data.results;
-      })
-    }
-  }
+    data() {
+        return {
+            baseUrl: "http://localhost:8000",
+            projects: []
+        };
+    },
+    mounted() {
+        this.getProject();
+    },
+    methods: {
+        getProject() {
+            console.log("get projects");
+            axios.get(`${this.baseUrl}/api/projects`).then(resp => {
+                this.projects = resp.data.results;
+            });
+        }
+    },
+    components: { ProjectCard }
 }
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <h2>Lista dei Progetti</h2>
+    <div class="row row-cols-3 g-3">
+      <div class="col" v-for="project in projects" :key="project.id">
+       <ProjectCard :project="project"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    },
+    data() {
+        return {
+            baseUrl: "http://localhost:8000",
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card h-100">
+        <img v-if="project.image" src="`${baseUrl}/storage/${project.image}`" class="card-img-top" 
+        :alt="project.title">
+        <div v-else>
+            Nessuna immagine presente
+        </div>
+        <div class="card-body">
+            <h4>{{ project.title }}</h4>  
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
